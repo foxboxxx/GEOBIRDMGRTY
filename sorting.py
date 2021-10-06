9a384beda08007b1ca8aa52c3ef325d54a9fd9d4
Joe <robertazzijoseph@gmail.com>
2021-10-05 23:35:35 -0400

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import statsmodels.graphics.api as smg
 import statsmodels.api as sm
 from scipy import stats
-#testing pushes
+#testing pushes @2
 
-
+print("...")
 print("Begininng Updating Sequence...")
 #testData = pd.read_csv(r"https://docs.google.com/spreadsheets/d/e/2PACX-1vQ1QvEdtQLKMGXxyN0QhQL46AdUdJ9iJVB-50NBHe1s_tnx2KCymG0ZYX43eB-SEjTl9Dj_5WHU8qj3/pub?output=csv")
 magneticData37 = pd.read_csv(r"magneticData37.csv")
