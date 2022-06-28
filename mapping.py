d7e622b73f77e4700ec4cc65ebe6833263d7fbec
foxboxxx <36267395+foxboxxx@users.noreply.github.com>
2022-06-28 14:37:16 -0400

    mapping update
    updated mapping and some regions

--- mapping.py
+++ mapping.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import pygmt
-import igrf
+import pyIGRF
 
 # mag = igrf.igrf('2010-07-12', glat=65, glon=-148, alt_km=100)
 pygmt.show_versions()
@@ -9,7 +9,7 @@ pygmt.show_versions()
 
 # igrfDataFull = pd.read_csv(r'igrfTestNoaaWeb.csv')
 
-worldRegion = [-170,180,-60,80]
+worldRegion = [-180,180,-90,90]
 
 # mag = igrf.igrf('2010-07-12', glat=65, glon=-148, alt_km=100)
 # spacing = "111km"
@@ -19,14 +19,77 @@ print(grid)
 grid2 = pygmt.xyz2grd(data = grid, region = worldRegion, spacing = "222km")
 # grd = pygmt.xyz2grd(data = igrfDataFull, x = igrfDataFull.long, y = igrfDataFull.lat, z = igrfDataFull.intensity)
 fig = pygmt.Figure()
-fig.grdimage(grid=grid2, projection = "R12c",)
+
+#just gradient for 1980
+fig.grdimage(grid=grid2, projection = 'H15c')
 print(grid)
 fig.show()
 
+#just contour lines for 1980
 fig = pygmt.Figure()
 fig.grdcontour(grid=grid2)
 fig.show()
 
+#full map put together with previous elements
+fig = pygmt.Figure()
+fig.basemap(region=worldRegion, projection="H15c", frame=True)
+fig.coast(land="#666666", water="lightblue", shorelines = True)
+fig.grdimage(grid=grid2, transparency=25, projection = 'H15c')
+# initial contour attempt
+# fig.grdcontour(grid=grid2, projection = 'H15c', interval = 2500)
+fig.contour(
+    pen="0.1p",
+    # pass the data as 3 1d data columns
+    x=grid['long'],
+    y=grid['lat'],
+    z=grid['intensity'],
+    # set the contours z values intervals to 1000
+    levels=1000,
+    # set the contours annotation intervals to 5000
+    annotation=5000,
+)
+fig.contour(
+    pen="0.2p,red",
+    # pass the data as 3 1d data columns
+    x=grid['long'],
+    y=grid['lat'],
+    z=grid['intensity'],
+    # set the contours z values intervals to 5000
+    levels=5000,
+)
+fig.show()
+
+# read in the xyz file into a 1D numpy array
+
+fig = pygmt.Figure()
+fig.basemap(region=[-180, 180, -90, 90], projection="H15c", frame=True)
+fig.coast(land="#666666", water="skyblue")
+
+fig.contour(
+    pen="0.1p",
+    # pass the data as 3 1d data columns
+    x=grid['long'],
+    y=grid['lat'],
+    z=grid['intensity'],
+    # set the contours z values intervals to 1000
+    levels=1000,
+    # set the contours annotation intervals to 5000
+    annotation=5000,
+)
+
+#make index contours red
+fig.contour(
+    pen="0.2p,red",
+    # pass the data as 3 1d data columns
+    x=grid['long'],
+    y=grid['lat'],
+    z=grid['intensity'],
+    # set the contours z values intervals to 5000
+    levels=5000,
+)
+
+
+fig.show()
 
 # grd = pygmt.xyz2grd(data = igrfDataFull, region = worldRegion, spacing = spacing)
 
