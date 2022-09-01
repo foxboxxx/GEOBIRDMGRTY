13630c954a1e904c321ae1ad10f648ec69c7e0ba
foxboxxx <36267395+foxboxxx@users.noreply.github.com>
2022-09-01 00:24:16 -0400

    Another slight update before the day of Reckoning
    the day has finally approached. the first day of senior year. before this eventful moment in my life, i am submitting one final update and then will go to sleep and wake up a senior. this one small change (literally one line) may not seem like a monumental change at face value, but to me it is the final mark of one stage of my life to a new exciting year which will also be my last year of high school. more updates soon.

--- massproduction.py
+++ massproduction.py
@@ -13,5 +13,6 @@ birdList = []
 for bird in birdList:
     rawFile = pd.read_csv('{}'.format(bird))
     rawFile = rawFile.loc[rawFile['occurrenceStatus'] == 'PRESENT', ['eventDate', 'individualCount', 'decimalLatitude', 'decimalLongitude', 'day', 'month', 'year']].copy()
+
     filteredFile = rawFile.query('0 > decimalLatitude >= -60 & -90 <= decimalLongitude <= -30 & individualCount < 1000')
-    
+    smallerRegion = rawFile.query('-25 >= decimalLatitude >= -35 & -55 >= decimalLongitude >= -65 & individualCount < 1000').copy()
