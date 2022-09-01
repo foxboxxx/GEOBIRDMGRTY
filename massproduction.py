ab2ff6606f9d93e1acd69d7f756d2542eb9dbcbc
foxboxxx <36267395+foxboxxx@users.noreply.github.com>
2022-09-01 16:34:45 -0400

    small update

--- massproduction.py
+++ massproduction.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib as matplotlib
 from scipy import stats
 import seaborn as sb
-from sorting import histogramMaker, doubleYAxisPlotMaker
+from sorting import histogramMaker, doubleYAxisPlotMaker, yearCount, monthCount
 
 birdList = []
 
@@ -14,5 +14,16 @@ for bird in birdList:
     rawFile = pd.read_csv('{}'.format(bird))
     rawFile = rawFile.loc[rawFile['occurrenceStatus'] == 'PRESENT', ['eventDate', 'individualCount', 'decimalLatitude', 'decimalLongitude', 'day', 'month', 'year']].copy()
 
-    filteredFile = rawFile.query('0 > decimalLatitude >= -60 & -90 <= decimalLongitude <= -30 & individualCount < 1000')
     smallerRegion = rawFile.query('-25 >= decimalLatitude >= -35 & -55 >= decimalLongitude >= -65 & individualCount < 1000').copy()
+    largerRegion = rawFile.query('0 > decimalLatitude >= -60 & -90 <= decimalLongitude <= -30 & individualCount < 1000')
+
+    smallCount = yearCount(smallCount, smallerRegion, 2000, 2020 )
+    largeCount = yearCount(largeCount, largerRegion, 2000, 2020)
+
+
+
+
+
+
+
+
